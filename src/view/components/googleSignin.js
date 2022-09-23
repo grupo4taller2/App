fe8fe142aead6f59d47781af65fd7b49bf6bf086
Fran <franciscopere987@gmail.com>
2022-09-23 20:08:29 -0300

    Log token on federated log in

--- src/view/components/googleSignin.js
+++ src/view/components/googleSignin.js
@@ -11,7 +11,12 @@ export default function GoogleLogin(props){
     const [request, response, promptAsync] = Google.useAuthRequest({
       expoClientId: '91621333154-2cccsnv6n0bo4iv18rokuur856oo6sdi.apps.googleusercontent.com'
     })
-    
+    React.useEffect(() => {
+      
+      if (response?.type === "success") {
+        console.log(response);
+      }
+    }, [response]);
     return (<Button style={[props.singInButton]} contentStyle={props.signInButtonContent} labelStyle={props.buttonText} uppercase={false} onPress={() => promptAsync()}>
       Google
     </Button>)
