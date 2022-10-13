5575a2c522bd71d6681fdf06a5f51cba40522efe
alejo-marino <tommy230201@gmail.com>
2022-10-13 14:37:02 -0300

    Connected with google's geocoding API, added route validator to TripScreen

--- src/view/screens/TripScreen.js
+++ src/view/screens/TripScreen.js
@@ -1,44 +1,76 @@
 import { React, useState } from 'react';
 import MapView from 'react-native-maps';
 import Marker from 'react-native-maps';
-import { SafeAreaView, StyleSheet, TouchableNativeFeedback, View, Dimensions } from "react-native";
-import { Text, Appbar, Avatar, Drawer, List, Menu, Surface, TextInput, PROVIDER_GOOGLE, Button, IconButton } from "react-native-paper";
+import { SafeAreaView, StyleSheet, TouchableNativeFeedback, View, Dimensions, SliderComponent } from "react-native";
+import { Text, Appbar, Avatar, Drawer, List, Menu, Surface, TextInput, PROVIDER_GOOGLE, Button, IconButton, Snackbar } from "react-native-paper";
 import Geocoder from 'react-native-geocoding';
 import { Location } from 'expo';
+import WalletView from '../composed/walletView';
 
 
 //In your code, import { PROVIDER_GOOGLE } from react-native-maps and add the property provider=PROVIDER_GOOGLE to your <MapView>. This property works on both iOS and Android.
 
-async function checkLocationValidity(location1, location2) {
+async function checkLocationValidity(location) {
     let geocodingAPIKey = "AIzaSyArMKi-b4wEAblYqFMF0B3XEc8nW9mZ9uE";
     Geocoder.init(geocodingAPIKey);
-    Geocoder.from("Colosseum")
-    .then(json => {
-        var location = json.results[0].geometry.location;
-        console.log(location);
-    })
-    .catch(error => console.warn(error));
+    try {
+        let response = await Geocoder.from(location);
+        /*var location_json = response.results[0].geometry.location;
+        console.log(location_json);*/
+        return true;
+    } catch (error) {   // will enter here if location is invalid
+        //console.warn(error);
+        return false;
+    }
 }
+
 
 export default function TripScreen({navigation}){
     const [start, setStart] = useState(''); // Aca se puede poner la locacion default del usuario si se la accede desde el contexto y tambien ponerla en el defaultValue del TextInput
     const [destination, setDestination] = useState('');
     const [region, setRegion] = useState({
-        latitude: 51.5079145,
-        longitude: -0.0899163,
+        latitude: 0.01,
+        longitude: 0.01,
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
     });
+    const [validTrip, setValidTrip] = useState(undefined);
+    const [tripCost, setTripCost] = useState("0 ETH");
+    const [visibleSB, setVisibleSB] = useState(false);
+    const [confirmedRoute, setConfirmedRoute] = useState(false);
 
+
+    const onToggleSnackBar = () => {setVisibleSB(!visibleSB); console.log("holis")}
+
+    const onDismissSnackBar = () => setVisibleSB(false);
+
+    async function checkTripValidity(start_location, destination_location) {
+        let validity1 = await checkLocationValidity(start_location);
+        let validity2 = await checkLocationValidity(destination_location);
+        
+        if (validity1 && validity2) {
+            console.log("Valid trip from: " + start_location + " to " + destination_location + ".");
+            setValidTrip(true);
+            // tripPrice = getPriceFromBack();
+            // setTripCost(tripPrice);
+        }
+        else {
+            setValidTrip(false);
+            console.log(visibleSB);
+            onToggleSnackBar();
+        }
+    }
+
+    
     return (
         <View style={styles.mainView}>
             <View style={styles.mapView}>
                 <MapView style={styles.map} 
                 initialRegion={{
-                latitude: -34.556,
-                longitude: -58.449,
-                latitudeDelta: 0.04,
-                longitudeDelta: 0.04,
+                latitude: -34.556,      // Estaria bueno que tanto la latitud como la longitud empiezen en la
+                longitude: -58.449,     // locacion default de la persona, si se trae en el contexto solo habria que meterla en el Geocoder y parsear la latitud y longitud de la response 
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01,
                 }}
                 showsTraffic={true} showsCompass={true} showsBuildings={true} showsIndoors={true}
                 onRegionChangeComplete={(region) => setRegion(region)}>
@@ -46,9 +78,22 @@ export default function TripScreen({navigation}){
                     <View style={styles.debugInfoView}>
                         <Text style={styles.text}>Current latitude: {region.latitude}</Text>
                         <Text style={styles.text}>Current longitude: {region.longitude}</Text>
-                        <Button buttonColor='#fff' style={styles.checkLocationButton} labelStyle={styles.checkLocationButtonLabel} contentStyle={styles.checkLocationButtonContent} icon="car"  onPress={() => {console.log('Pressed'), checkLocationValidity(start, destination).catch().then()}}>
-                            Start Trip
+                        <Button buttonColor='#50C878' mode='contained' style={styles.checkLocationButton} labelStyle={styles.checkLocationButtonLabel} contentStyle={styles.checkLocationButtonContent}
+                            icon="navigation-variant"  onPress={() => {console.log('Pressed'), checkTripValidity(start, destination)}}>
+                            Set Route
                         </Button>
+                        <Button buttonColor='#000' mode='contained' style={styles.checkLocationButton} labelStyle={styles.checkLocationButtonLabel} contentStyle={styles.checkLocationButtonContent}
+                            icon="car" disabled={!validTrip} onPress={() => {/*//startTrip();  aca returnear el componente OngoingTripScreen, va realmente habria que hacer una
+                            checkbox para que el usuario vea el costo y valide el viaje*/}}>
+                            Start Trip for {tripCost}
+                        </Button>
+                        <Snackbar
+                            visible={visibleSB}
+                            onDismiss={onDismissSnackBar}
+                            duration='2000'
+                            style={styles.snackbar}>
+                            Invalid route. Check start and destination!
+                        </Snackbar>
                     </View>
                 </MapView>
             </View>
@@ -87,10 +132,21 @@ const styles = StyleSheet.create({
     checkLocationButtonContent: {
     },
     checkLocationButtonLabel: {
-        color: '#000'
+        color: '#fff'
+    },
+    startTripButton: {
+        marginLeft: 400
+    },
+    startTripButtonContent: {
+    },
+    startTripButtonLabel: {
+        color: '#fff'
     },
     debugInfoView: {
         marginTop: 500,
         alignItems: 'center'
+    },
+    snackbar: {
+        backgroundColor: '#D22B2B'
     },
 })
